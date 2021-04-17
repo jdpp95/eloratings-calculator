deb3c565db1c2196accf69e7c6466adb80013213
jdpp95 <jdporrasp@unal.edu.co>
2021-04-17 17:53:11 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+require('colors')
+
+console.log('----------------------------------'.blue)
+console.log('|     ELO Ratings calculator     |'.blue)
+console.log('----------------------------------'.blue)
